9f35a8b32ed46b839cec4071f69fbb663f78ecf3
Scott Hatch <scooter11961@gmail.com>
2013-08-05 15:36:21 -0600

    Initial checkin

---
+++ app/controllers/Application.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+
+object Application extends Controller {
+  
+  def index = Action {
+//    Ok(views.html.index("Your new application is ready."))
+//    Ok(views.html.index("Hello World"))
+    Redirect(routes.Application.tasks)
+  }
+
+  import models.Task
+
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
+
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
+
+  def deleteTask(id: Long) = TODO
+
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+
+
+
+}
---
+++ app/models/Task.scala
@@ -0,0 +1,15 @@
+package models
+import play.api.db._
+import play.api.Play.current
+
+ case class Task(id: Long, label: String)
+
+object Task {
+
+  //def all(): List[Task] = Nil
+  //def create(label: String) {}
+  //def delete(id: Long) {}
+
+
+
+}
